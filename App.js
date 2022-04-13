24244949de90253af9ae0a365ae6976773de99f5
nhathuan10 <67364775+nhathuan10@users.noreply.github.com>
2022-04-13 12:15:36 +0700

    new

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { Provider } from 'react-redux'
+import store from './src/redux/rootStore'
+import RootNavigation from './src/navigation/rootNavigation'
 
 export default function App() {
-  return (
+  return(
+    // <Provider store={store}>
+    //   <NavigationContainer>
+    //     <RootNavigation />
+    //   </NavigationContainer>
+    // </Provider>
     <View>
-      <Text>App</Text>
+      <Text>asd</Text>
     </View>
   )
 }
---
+++ src/utils/GlobalStyle/index.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    GlobalFont: {
+        fontFamily: 'Nunito-Bold',
+        fontSize: 20,
+    }
+})
