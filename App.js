4987e08560a25583ea20181ce65a83f17512b312
Huan <hphan298@gmail.com>
2022-05-03 19:17:33 +0700

    1

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { Provider } from 'react-redux'
@@ -6,13 +6,13 @@ import store from './src/redux/rootStore'
 import RootNavigation from './src/navigation/rootNavigation'
 
 export default function App() {
-  return(
+  return (
     <Provider store={store}>
       <NavigationContainer>
         <RootNavigation />
       </NavigationContainer>
     </Provider>
-    )
+  )
 }
 
 const styles = StyleSheet.create({})
