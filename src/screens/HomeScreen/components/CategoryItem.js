beef7b878da7280001a6e5b892e20a4e0a9bc0d2
Huan <hphan298@gmail.com>
2022-05-10 00:16:27 +0700

    1

--- src/screens/HomeScreen/components/CategoryItem.js
+++ src/screens/HomeScreen/components/CategoryItem.js
@@ -1,5 +1,5 @@
 import { StyleSheet, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import React from 'react'
 import { Text } from '../../../components'
 import { COLORS } from '../../../themes'
 
